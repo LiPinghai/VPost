9081cf5f03b963893d3e92918e12b11c1acc776f
pinghai <twopoles@163.com>
2016-05-26 15:56:28 +0800

    update

--- test/test.js
+++ test/test.js
@@ -1,44 +1,50 @@
-var VPost = require("./lib/VPost");
+var VPost = require("../lib/VPost");
 
 var config = {
-	rootPath:"",
-	data:[
-		{
-			"req":"/getString",
-			"res":"baobao"
+		rootPath: "",
+		data: [{
+			"req": "getString",
+			"res": "string",
 		},
 		{
-			"req":"/getFuntion",
-			"res":function(){
-				return "{'id':'" + String(Math.random())+"'}" 
-			},
-			"code":"201"
+			"req": "/getFuntion",
+			"res": function() {
+				return '{"funtion":"' + String(Math.random()) + '"}'
+			}
 		},
 		{
-			"req":"/getArray",
-			"res":[1,2,3,4,5]
+			"req": "/getArray",
+			"res": [1, 2, 3, 4, 5]
 		},
 		{
-			"req":"/getObject",
-			"res":{
-				a:"1",
-				b:[1,2,3,4],
-				c:function(){
-					return "c"
+			"req": "/getObject",
+			"res": {
+				a: "a",
+				b: ["b1", "b2", "b3", "b4", "b5"],
+				c: function() {
+					return "c";
+				},
+				d: {
+					d1: "d1",
+					d2: ["d21", "d22", "d23"],
+					d3: function() {
+						return "d3"
+					},
+					d4: {
+						d41: "d41"
+					}
 				}
 			}
 		}
 	],
-	default:"not found",
-	code:[
-		{404:"2222"},
-		{201:"12321321"}
-	],
-	port:"2048"
-	
-} 
+	default: "not found",
+	code: [{
+		404: ""
+	}, {
+		201: ""
+	}],
+	port: "2048"
 
+}
 
 var vPost = new VPost(config);
-
-
